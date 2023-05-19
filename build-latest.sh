51f908976f5b2516a676046ba76c8fe44d274eef
kitobor-ai <133595216+kitobor-ai@users.noreply.github.com>
2023-05-19 16:20:56 +0200

    Create build-latest.sh

---
+++ build-latest.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+
+# docker buildx create --name ki_builder
+# docker buildx create --name ki_builder --driver-opt env.BUILDKIT_STEP_LOG_MAX_SIZE=50000000,env.BUILDKIT_STEP_LOG_MAX_SPEED=100000000 --use
+docker buildx use ki_builder
+
+# load cross-platform emulator
+docker run --privileged --rm tonistiigi/binfmt --install all
+
+# Make sure to always have fresh base image
+docker pull debian:11
+
+# Build dev image with just binaries
+docker buildx build --pull --rm --platform linux/arm64,linux/amd64 -t kitobor/ki-gpt:latest -f Dockerfile.dev "." --push
+
+# docker buildx build --pull --rm -f Dockerfile.export --platform linux/arm64,linux/amd64 -t kitobor/ki-gpt:latest --output out .
